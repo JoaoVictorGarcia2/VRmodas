8dd03eba9949773ce133074df411305303e989eb
João Victor Garcia <joao.garcia@gpnet.com.br>
2023-11-01 10:46:49 -0300

    Finalização total do projeto.

--- src/componentes/Carrossel/index.js
+++ src/componentes/Carrossel/index.js
@@ -1,12 +1,13 @@
-import {useRef} from 'react';
+import {useRef, useEffect, useState} from 'react';
 import './Carrossel.css';
 import seta from './img/arrow.png'
 import Img1 from './img/carrocel-1.png';
 import Img2 from './img/carrocel-2.png';
 import Img3 from './img/carrocel-3.png';
 
-function App() {
+function Carrosel() {
   const carousel = useRef(null);
+  const [isLastImage, setIsLastImage] = useState(false);
 
 
   //Função de scroll do carrossel
@@ -16,20 +17,37 @@ function App() {
   };
   const seta_direita = (e) => {
     e.preventDefault();
-    carousel.current.scrollLeft += carousel.current.offsetWidth;
+    if (carousel.current.scrollLeft + carousel.current.offsetWidth >= carousel.current.scrollWidth) {
+      setIsLastImage(true);
+    } else {
+      setIsLastImage(false);
+      carousel.current.scrollLeft += carousel.current.offsetWidth;
+    }
   };
   //Fim da função.
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isLastImage) {
+        setIsLastImage(false);
+        carousel.current.scrollLeft = 0; // Volta para a primeira imagem
+      } else {
+        carousel.current.scrollLeft += carousel.current.offsetWidth;
+      }
+    }, 4000);
+
+    return () => clearInterval(interval);
+    }, [isLastImage]);
 
   return (
     <div className="container">
 
       <div className="carousel" ref={carousel}>
 
-        <div className='rosel'>
-          <div className="item"><img src={Img1}></img></div>
-          <div className="item"><img src={Img2}></img></div>
-          <div className="item"><img src={Img3}></img></div>
+        <div className='rosel' ref={carousel}>
+          <div className="item"><img src={Img1} alt="Imagem 1"></img></div>
+          <div className="item"><img src={Img2} alt="Imagem 2"></img></div>
+          <div className="item"><img src={Img3} alt="Imagem 3"></img></div>
         </div>
         
       </div>
@@ -45,4 +63,4 @@ function App() {
   );
 }
 
-export default App;
+export default Carrosel;
